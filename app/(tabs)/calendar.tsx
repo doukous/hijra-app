045ab603763fdd87e7c555be48cd9cef8a046ec9
Mohamed Doucouré <mohamed.doucoure@protonmail.com>
2026-06-20 23:32:09 +0000

    start implementing swipe handling with react native gesture handler

--- app/(tabs)/calendar.tsx
+++ app/(tabs)/calendar.tsx
@@ -4,6 +4,9 @@ import { CustomDate, daysInitials } from "@/utils/date-helpers";
 import { ChevronLeft, ChevronRight } from "lucide-react-native";
 import { useEffect, useState } from "react";
 import { View, Text, Pressable } from "react-native";
+import { Directions, useFlingGesture } from "react-native-gesture-handler";
+import { GestureDetector } from "react-native-gesture-handler/lib/typescript/handlers/gestures/GestureDetector";
+import Animated, { useSharedValue, withTiming } from "react-native-reanimated";
 
 export default function CalendarScreen() {
   const customDate = new CustomDate();
@@ -13,8 +16,16 @@ export default function CalendarScreen() {
   );
 
   const [isdateSectionToggled, toggleDateSection] = useState<boolean>(false);
-
   const [calendarTable, setCalendarTable] = useState(new Array(35).fill(null));
+
+  const position = useSharedValue(0);
+
+  const flingGesture = useFlingGesture({
+    direction: Directions.UP,
+    onActivate: () => {
+      position.value = withTiming(position.value + 10, { duration: 100 });
+    },
+  });
 
   useEffect(() => {
     const table = buildArrayMonth();
@@ -45,55 +56,57 @@ export default function CalendarScreen() {
 
   return (
     <View className="flex-1 items-center justify-around">
-      <View className="w-full flex-row items-center justify-between gap-x-8 px-4">
-        <Pressable
-          className="bg-blue-400 p-2 rounded-xl"
-          onPress={() => customDate.previousMonth()}
-        >
-          <ChevronLeft />
-        </Pressable>
+      <GestureDetector gesture={flingGesture}>
+        <View className="w-full flex-row items-center justify-between gap-x-8 px-4">
+          <Pressable
+            className="bg-blue-400 p-2 rounded-xl"
+            onPress={() => customDate.previousMonth()}
+          >
+            <ChevronLeft />
+          </Pressable>
 
-        <Text className="w-2/5">
-          {date.monthInEnglish.toUpperCase()} - {date.monthInArabic}
-        </Text>
+          <Text className="w-2/5">
+            {date.monthInEnglish.toUpperCase()} - {date.monthInArabic}
+          </Text>
 
-        <Text>{date.year} AH</Text>
+          <Text>{date.year} AH</Text>
 
-        <Pressable
-          className="bg-blue-400 p-2 rounded-xl"
-          onPress={() => customDate.nextMonth()}
-        >
-          <ChevronRight />
-        </Pressable>
-      </View>
-
-      <View className="gap-y-2">
-        <View className="flex flex-row flex-wrap justify-start w-full bg-emerald-500">
-          {daysInitials.map((value, key) => (
-            <Text key={key} className="text-white m-auto">
-              {value}
-            </Text>
-          ))}
+          <Pressable
+            className="bg-blue-400 p-2 rounded-xl"
+            onPress={() => customDate.nextMonth()}
+          >
+            <ChevronRight />
+          </Pressable>
         </View>
 
-        <View className="flex flex-row flex-wrap justify-start w-full bg-amber-400">
-          {calendarTable.map((value, key) => (
-            <Pressable
-              key={key}
-              className={`bg-blue-700 w-[14.28%] ${isdateSectionToggled ? "h-16" : "h-24"}`}
-              onPress={() => toggleDateSection(true)}
-            >
-              <Text className="text-white m-auto">{value}</Text>
-            </Pressable>
-          ))}
-        </View>
-      </View>
+        <View className="gap-y-2">
+          <View className="flex flex-row flex-wrap justify-start w-full bg-emerald-500">
+            {daysInitials.map((value, key) => (
+              <Text key={key} className="text-white m-auto">
+                {value}
+              </Text>
+            ))}
+          </View>
 
-      {isdateSectionToggled && (
-        <View className="h-1/3 w-full bg-emerald-400">
-          <Text>Date</Text>
+          <Animated.View className="flex flex-row flex-wrap justify-start w-full bg-amber-400">
+            {calendarTable.map((value, key) => (
+              <Pressable
+                key={key}
+                className="bg-blue-700 w-[14.28%]"
+                onPress={() => toggleDateSection(true)}
+              >
+                <Text className="text-white m-auto">{value}</Text>
+              </Pressable>
+            ))}
+          </Animated.View>
         </View>
-      )}
+
+        {isdateSectionToggled && (
+          <Animated.View className="h-1/3 w-full bg-emerald-400">
+            <Text>Date</Text>
+          </Animated.View>
+        )}
+      </GestureDetector>
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,13 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "@/global.css";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function RootLayout() {
   const insets = useSafeAreaInsets();
 
   return (
-    <>
+    <GestureHandlerRootView>
       <Stack>
         <Stack.Screen
           name="(tabs)"
@@ -18,6 +19,6 @@ export default function RootLayout() {
         />
       </Stack>
       <StatusBar style="auto" />
-    </>
+    </GestureHandlerRootView>
   );
 }
