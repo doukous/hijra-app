3cb6b02e632e6a825f137fd90d776bf7578217bf
Mohamed Doucouré <mohamed.doucoure@protonmail.com>
2026-06-23 19:17:53 +0000

    update the component

--- components/calendar/header.tsx
+++ components/calendar/header.tsx
@@ -1,14 +1,32 @@
+import { GenericDateData } from "@/modules/calendar-bridge/src/CalendarBridge.types";
 import { ChevronLeft, ChevronRight } from "lucide-react-native";
 import { Pressable, View, Text } from "react-native";
 
-export default function Header(month: string) {
+export default function Header({
+  date,
+  toPreviousMonth,
+  toNextMonth,
+}: {
+  toPreviousMonth: () => void;
+  toNextMonth: () => void;
+  date: GenericDateData;
+}) {
   return (
-    <View className="flex-row items-center gap-x-6">
-      <Pressable>
+    <View className="w-full flex-row items-center justify-between gap-x-8 px-4">
+      <Pressable
+        className="bg-blue-400 p-2 rounded-xl"
+        onPress={toPreviousMonth}
+      >
         <ChevronLeft />
       </Pressable>
-      <Text className="text-xl">{month}</Text>
-      <Pressable>
+
+      <Text className="w-2/5">
+        {date.monthInEnglish.toUpperCase()} - {date.monthInArabic}
+      </Text>
+
+      <Text>{date.year} AH</Text>
+
+      <Pressable className="bg-blue-400 p-2 rounded-xl" onPress={toNextMonth}>
         <ChevronRight />
       </Pressable>
     </View>
